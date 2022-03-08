a522171236d973894de1fb37d6ae5376d9cadde0
KokumaiLuis <lgkokumai@gmail.com>
2022-03-08 14:21:05 -0300

    first commit - add files

---
+++ AULA_01/Aula_01.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+print(name)
+
+var name: String = "Luis"
+var lastName: String = "Gustavo"
+var greeting = "Hello, playground"
+
+print("\(name) \(lastName)")
+
+
+var tuple: (String, Int) = ("Luis", 10)
+
+tuple.0 = ("Luisa")
+
+print(tuple)
