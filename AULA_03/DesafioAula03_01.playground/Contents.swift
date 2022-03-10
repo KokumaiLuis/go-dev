a5eff6efce3464692f44429a710feec65c47c130
SP11793 <sp11793@MacBook-Pro-de-SP11793.local>
2022-03-10 19:53:01 -0300

    DesafioAula03_01 atualizado. Aula04_01 adicionada

--- AULA_03/DesafioAula03_01.playground/Contents.swift
+++ AULA_03/DesafioAula03_01.playground/Contents.swift
@@ -4,31 +4,33 @@ import UIKit
 
 //Funcao com retorno
 func isPalindrome1(word: String) -> Bool{
-    let reversedWord: String = String(word.reversed())
+    let reversedWord: String = String(word.filter({$0 != " "}).reversed())
+    let wordNoSpace: String = String(word.filter({$0 != " "}))
     
-    if (reversedWord == word && reversedWord != "") {
+    if (reversedWord == wordNoSpace && reversedWord != "") {
         return true
     } else {
         return false
     }
 }
 
-print(isPalindrome1(word: "subinoonibus"))
+print(isPalindrome1(word: "subi no onibus"))
 print(isPalindrome1(word: "luis"))
 
 
 //Funcao sem retorno
 func isPalindrome2(word: String) {
-    let reversedWord: String = String(word.reversed())
+    let reversedWord: String = String(word.filter({$0 != " "}).reversed())
+    let wordNoSpace: String = String(word.filter({$0 != " "}))
     
-    if (reversedWord == word && reversedWord != "") {
-        print("\(word) = \(reversedWord)")
-        print("A palavra '\(word)' é palíndromo.")
+    if (reversedWord == wordNoSpace && reversedWord != "") {
+        print("\(wordNoSpace) = \(reversedWord)")
+        print("A palavra '\(wordNoSpace)' é palíndromo.")
     } else {
-        print("\(word) != \(reversedWord)")
-        print("A palavra '\(word)' não é palíndromo.")
+        print("\(wordNoSpace) != \(reversedWord)")
+        print("A palavra '\(wordNoSpace)' não é palíndromo.")
     }
 }
 
-isPalindrome2(word: "subinoonibus")
+isPalindrome2(word: "subi no onibus")
 isPalindrome2(word: "luis")
