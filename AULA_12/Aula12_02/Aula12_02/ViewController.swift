e4c077a53ef26ee7a81d14251f287a10d08cde4f
KokumaiLuis <lgkokumai@gmail.com>
2022-03-23 21:32:59 -0300

    Aula12_02 modificada

--- AULA_12/Aula12_02/Aula12_02/ViewController.swift
+++ AULA_12/Aula12_02/Aula12_02/ViewController.swift
@@ -9,7 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//    var button: UIButton = UIButton()
+
+    private enum Defaults {
+        
+        static let textInfo = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        
+        static let textButton = "Call View"
+        static let textLabel = "Label centralizada"
+    }
+    
     var safeArea: UILayoutGuide!
     
     lazy var iconImageView: UIImageView = {
@@ -24,11 +32,92 @@ class ViewController: UIViewController {
         label.textColor = .black
         label.numberOfLines = 0
         label.font = UIFont.systemFont(ofSize: 13)
-        label.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        label.text = Defaults.textInfo
         label.textAlignment = .left
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    lazy var callButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle(Defaults.textButton, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.addTarget(self, action: #selector(getView), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    lazy var content: UIView = {
+       let view = UIView()
+        view.backgroundColor = .systemIndigo
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textColor = .white
+        label.text = Defaults.textLabel
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    @objc func getView() {
+        print("Botão clicado")
+    }
+    
+    // MARK: - Private Methods
+    
+    private func addSubViews() {
+        view.addSubview(iconImageView)
+        view.addSubview(titleLabel)
+        view.addSubview(callButton)
+        view.addSubview(content)
+        content.addSubview(descriptionLabel)
+    }
+    
+    private func configureIconImageView() {
+        NSLayoutConstraint.activate([
+            iconImageView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 10),
+            iconImageView.heightAnchor.constraint(equalToConstant: 70),
+            iconImageView.widthAnchor.constraint(equalToConstant: 100),
+            iconImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    private func configureTitleLabel() {
+        titleLabel.topAnchor.constraint(equalTo: iconImageView.bottomAnchor, constant: Metrics.Margin.default).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Metrics.Margin.default).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Metrics.Margin.default).isActive = true
+    }
+    
+    private func configureCallButton() {
+        NSLayoutConstraint.activate([
+            callButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: Metrics.Margin.default),
+            callButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Metrics.Margin.default),
+            callButton.heightAnchor.constraint(equalToConstant: Metrics.Margin.input),
+            callButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Metrics.Margin.default)
+        ])
+    }
+    
+    private func configureContentView() {
+        NSLayoutConstraint.activate([
+            content.topAnchor.constraint(equalTo: callButton.bottomAnchor, constant: Metrics.Margin.default),
+            content.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Metrics.Margin.default),
+            content.heightAnchor.constraint(equalToConstant: 200),
+            content.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Metrics.Margin.default)
+        ])
+    }
+    
+    private func configureDescriptionLabel() {
+        NSLayoutConstraint.activate([
+            descriptionLabel.centerXAnchor.constraint(equalTo: content.centerXAnchor),
+            descriptionLabel.centerYAnchor.constraint(equalTo: content.centerYAnchor)
+        ])
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,24 +130,16 @@ class ViewController: UIViewController {
 //
 //        view.addSubview(button)
         
-        view.addSubview(iconImageView)
-        view.addSubview(titleLabel)
-        
-        NSLayoutConstraint.activate([
-            iconImageView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 10),
-            iconImageView.heightAnchor.constraint(equalToConstant: 70),
-            iconImageView.widthAnchor.constraint(equalToConstant: 100),
-            iconImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        ])
-        
-        
-        titleLabel.topAnchor.constraint(equalTo: iconImageView.bottomAnchor, constant: 16).isActive = true
-        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        addSubViews()
+        configureIconImageView()
+        configureTitleLabel()
+        configureCallButton()
+        configureContentView()
+        configureDescriptionLabel()
         
         
         title = "Using View Code"
-        view.backgroundColor = .lightGray
+        view.backgroundColor = .white
     }
 
 
